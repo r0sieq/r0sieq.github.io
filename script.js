1fcec017206a57b3273290e28ad4b725845bf7c2
rolandmusic <105501251+rolandmusic@users.noreply.github.com>
2023-06-30 13:25:23 +0200

    adding weathernow app

--- script.js
+++
@@ -1,8 +0,0 @@
-const OPTION = document.querySelectorAll('.option');
-
-OPTION.forEach( e => {
-    e.addEventListener('click', f => {
-        e.lastElementChild.click();
-        console.log(e.lastElementChild);
-    });
-});
